296c41510e0ebd568e6b64ddbcfe9cd2626655b8
TedBits <tedbits@mailbox.org>
2020-12-12 21:56:35 +0100

    add day9

---
+++ src/AdventOfCode/Day9/PuzzleDay9.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day9
+{
+    public class PuzzleDay9 : PuzzleBase, IPuzzle
+    {
+        public override int DayNumber => 9;
+
+        private List<long> DataList { get; } = new List<long>();
+        private int NumberToConsider { get; } = 25;
+
+        public void Load()
+        {
+            DataList.AddRange(File.ReadAllLines(FilePath).Select(x => long.Parse(x)).ToList());
+        }
+
+        public void Solve()
+        {
+            var faultingNumber = PartOne();
+            Console.WriteLine($"Part One: {faultingNumber}");
+            Console.WriteLine($"Part Two: {PartTwo(faultingNumber)}");
+        }
+
+        private long PartOne()
+        {
+            var firstPosition = NumberToConsider;
+            long faultingNumber;
+
+            while (true)
+            {
+                faultingNumber = DataList[firstPosition];
+                if (!SumFound(faultingNumber, firstPosition))
+                {
+                    break;
+                }
+                firstPosition++;
+            }
+
+            return faultingNumber;
+        }
+
+        private long PartTwo(long faultingNumber)
+        {
+            var firstPosition = 0;
+            var secondPosition = 1;
+
+            while (true)
+            {
+                var listToCheck = DataList.GetRange(firstPosition, secondPosition - firstPosition);
+                var rangeResult = listToCheck.Sum();
+                if (rangeResult == faultingNumber)
+                {
+                    return listToCheck.Min() + listToCheck.Max();
+                }
+
+                if (rangeResult < faultingNumber)
+                {
+                    secondPosition++;
+                }
+                else
+                {
+                    firstPosition++;
+                }
+            }
+        }
+
+        private bool SumFound(long nextNumberToCheck, int position)
+        {
+            var workingList = DataList.Skip(position - NumberToConsider).Take(NumberToConsider).ToList();
+            workingList.Sort();
+            for (int i = 0; i < NumberToConsider - 1; i++)
+            {
+                var sum = nextNumberToCheck - workingList[i];
+                var result = workingList.BinarySearch(sum);
+                if (result >= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
