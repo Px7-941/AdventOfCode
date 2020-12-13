0421c1f8d48d585562a08b9175aa0f4c61265184
TedBits <tedbits@mailbox.org>
2020-12-13 23:12:57 +0100

    add day12

---
+++ src/AdventOfCode/Day12/PuzzleDay12.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day12
+{
+    public partial class PuzzleDay12 : PuzzleBase, IPuzzle
+    {
+        public override int DayNumber => 12;
+
+        private List<string> Data { get; set; }
+
+        public void Load()
+        {
+            Data = File.ReadLines(FilePath).ToList();
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine($"Part One: {PartOne()}");
+            Console.WriteLine($"Part Two: {PartTwo()}");
+        }
+
+        private long PartOne()
+        {
+            var ship = new Ship();
+            foreach (var (op, val) in Data.Select(l => (l[0], int.Parse(l[1..]))))
+            {
+                switch (op)
+                {
+                    case 'F':
+                        ship.MoveForward(val);
+                        break;
+                    case 'E':
+                        ship.Move(0, val);
+                        break;
+                    case 'S':
+                        ship.Move(1, val);
+                        break;
+                    case 'W':
+                        ship.Move(2, val);
+                        break;
+                    case 'N':
+                        ship.Move(3, val);
+                        break;
+                    case 'R':
+                        ship.Rotate(val / 90);
+                        break;
+                    case 'L':
+                        ship.Rotate(4 - val / 90);
+                        break;
+                }
+            }
+
+            return ship.ManhattanDistance();
+        }
+
+        private long PartTwo()
+        {
+            var ship = new Ship();
+            foreach (var (op, val) in Data.Select(l => (l[0], int.Parse(l[1..]))))
+            {
+                switch (op)
+                {
+                    case 'F':
+                        ship.MoveForwardWaypoint(val);
+                        break;
+                    case 'E':
+                        ship.MoveWaypoint(0, val);
+                        break;
+                    case 'S':
+                        ship.MoveWaypoint(1, val);
+                        break;
+                    case 'W':
+                        ship.MoveWaypoint(2, val);
+                        break;
+                    case 'N':
+                        ship.MoveWaypoint(3, val);
+                        break;
+                    case 'R':
+                        ship.RotateWaypoint(val / 90);
+                        break;
+                    case 'L':
+                        ship.RotateWaypoint(4 - val / 90);
+                        break;
+                }
+            }
+
+            return ship.ManhattanDistance();
+        }
+    }
+}
---
+++ src/AdventOfCode/Day12/Ship.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace AdventOfCode.Day12
+{
+    public partial class PuzzleDay12
+    {
+        public sealed class Ship
+        {
+            private int X { get; set; }
+            private int Y { get; set; }
+            public int Dir { get; set; }
+            public int WayX { get; set; } = 10;
+            public int WayY { get; set; } = 1;
+
+            public void Move(int dir, int val)
+            {
+                (X, Y) = dir switch
+                {
+                    0 => (X + val, Y),
+                    1 => (X, Y - val),
+                    2 => (X - val, Y),
+                    3 => (X, Y + val),
+                    _ => (X, Y)
+                };
+            }
+
+            public void MoveWaypoint(int dir, int val)
+            {
+                (WayX, WayY) = dir switch
+                {
+                    0 => (WayX + val, WayY),
+                    1 => (WayX, WayY - val),
+                    2 => (WayX - val, WayY),
+                    3 => (WayX, WayY + val),
+                    _ => (WayX, WayY)
+                };
+            }
+
+            public void MoveForward(int val)
+            {
+                Move(Dir, val);
+            }
+
+            public void MoveForwardWaypoint(int val)
+            {
+                X += WayX * val;
+                Y += WayY * val;
+            }
+
+            public void Rotate(int val)
+            {
+                Dir = (Dir + val) % 4;
+            }
+
+            public void RotateWaypoint(int val)
+            {
+                (WayX, WayY) = val switch
+                {
+                    0 => (WayX, WayY),
+                    1 => (WayY, -WayX),
+                    2 => (-WayX, -WayY),
+                    3 => (-WayY, WayX),
+                    _ => (WayX, WayY)
+                };
+            }
+
+            public long ManhattanDistance()
+            {
+                return (Math.Abs(X) + Math.Abs(Y));
+            }
+        }
+    }
+}
